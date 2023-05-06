55797d6234467c08171dd9ecc2a9def7bcf10fb0
kailamcd <mcd.kaila@gmail.com>
2023-05-06 17:26:32 -0500

    added footer and script

---
+++ lesson3/scripts/script.js
@@ -0,0 +1,11 @@
+// make date info
+const options = {day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+let date = new Date().toLocaleDateString('en-US', options)
+const optionsYear = {year: 'numeric'};
+let year = new Date().toLocaleDateString('en-Us', optionsYear);
+
+// output data to html
+document.querySelector('#footer-info').innerHTML = `\u00A9${year}  |  Kaila McDonald  |  Texas`; 
+document.querySelector('#updated').innerHTML = `Last Updated ${date}`;
+
+//make assignment list
