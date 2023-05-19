4bd10ef09811c95ba57922c920ae643c5b168c08
kailamcd <mcd.kaila@gmail.com>
2023-05-18 22:04:02 -0500

    used ids

--- lesson4/scripts/script.js
+++ lesson4/scripts/script.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('#submit');
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 
 const li = document.createElement("li");
 
