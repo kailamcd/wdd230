d74c2008902c901b6e88a6a7be84114beb058b5e
kailamcd <mcd.kaila@gmail.com>
2023-05-26 22:48:23 -0500

    weather info javascript

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,18 @@
+temp = document.querySelector(".temp-title");
+temperature = 33;
+t = 5;
+s = 3;
+document.querySelector("#wind-speed").textContent = s;
+temp.textContent = `${temperature} F`;
+windchill = (35.74 + (0.6215 * t) - (35.75 * (s ** 0.16)) + (0.4275 * t * (s ** 0.16)));
+
+if (temperature <= 50 && s >= 3) {
+    windchill = Math.round(windchill);
+} else {
+    windchill = `Windchill: N/A`;
+}
+
+
+
+chill = document.querySelector(".chill");
+chill.textContent = `Windchill: ${windchill}`;
