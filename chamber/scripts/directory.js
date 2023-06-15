106957e3c3b578945422f4f43b400027366d198d
kailamcd <mcd.kaila@gmail.com>
2023-06-15 12:17:48 -0500

    building js

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,7 @@
 const busInfo = 'https://kailamcd.github.io/wdd230/chamber/json/data.json';
 
+console.log('js connected');
+
 async function getBusData(busInfo) {
     const response = await fetch(busInfo);
     if (response.ok) {
