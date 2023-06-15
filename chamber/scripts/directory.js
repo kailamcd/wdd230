bd31c14b21d08671ac8ee992eb60e0c857fe0cc1
kailamcd <mcd.kaila@gmail.com>
2023-06-15 18:28:28 -0500

    icons

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -7,7 +7,32 @@ async function getBusData(busInfo) {
     if (response.ok) {
         const data = await response.json();
         console.log(data.businesses);
+        createCards(data.businesses);
     }
 }
 
 getBusData(busInfo);
+
+const createCards = (business) => {
+    const cards = document.querySelector('#business-cards');
+
+    business.forEach(business => {
+        let container = document.createElement('section');
+        let icon = document.createElement('img');
+        let busName = document.createElement('h2');
+        let fullInfo = document.createElement('p');
+        console.log(business.logo);
+        icon.setAttribute('src', business.logo);
+        icon.setAttribute('alt', `Logo for ${business.name}`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '200');
+        icon.setAttribute('height', '200');
+
+        container.appendChild(icon);
+        container.appendChild(busName);
+        container.appendChild(fullInfo);
+
+        cards.appendChild(container);
+        
+    });
+}
