1785ef10e56cd18f5d4ae6d62882cc6c7c1e3c27
kailamcd <mcd.kaila@gmail.com>
2023-06-15 12:16:57 -0500

    begin js code for directory cards

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,1 +1,9 @@
-const bus-info = 
+const busInfo = 'https://kailamcd.github.io/wdd230/chamber/json/data.json';
+
+async function getBusData(busInfo) {
+    const response = await fetch(busInfo);
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data.businesses);
+    }
+}
