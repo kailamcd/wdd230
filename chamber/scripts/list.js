e7c8704c21fd121263089943588b58d66fc5af19
kailamcd <mcd.kaila@gmail.com>
2023-06-17 21:30:55 -0500

    list complete

--- chamber/scripts/list.js
+++ chamber/scripts/list.js
@@ -17,46 +17,38 @@ const createList = (business) => {
     table.style.display = 'block';
     table.style.border = "2px solid #097b02"
     table.style.maxWidth = 'fit-content';
-    let heading = document.createElement('tr');
-    let nameHead = document.createElement('th');
-    let addHead = document.createElement('th');
-    let phoneHead = document.createElement('th');
-    let webHead = document.createElement('th');
 
-    nameHead.textContent = 'Name';
-
-    if (window.innerWidth < 300) {
-        
-    }
-    addHead.textContent = 'Address';
-    phoneHead.textContent = 'Phone Number';
-    webHead.textContent = 'Website';
-
-    heading.appendChild(nameHead);
-    heading.appendChild(addHead);
-    heading.appendChild(phoneHead);
-    heading.appendChild(webHead);
-
-    table.appendChild(heading);
 
     business.forEach(business => {
         let row = document.createElement('tr');
         let compName = document.createElement('th');
         let address = document.createElement('td');
         let phoneNum = document.createElement('td');
+        let row2 = document.createElement('tr');
         let website = document.createElement('td');
+        let blankRow= document.createElement('tr');
 
         compName.innerHTML = `${business.name}`;
         address.innerHTML = `${business.address}`;
+
         phoneNum.textContent = `${business.phone}`;
+
         website.innerHTML = `<a href='${business.url}'>${business.url}</a>`;
         website.style.textAlign = 'center';
+        website.setAttribute('colspan', '3');
+
+        blankRow.setAttribute('colspan', '3');
+        blankRow.setAttribute('height', '20');
 
         row.appendChild(compName);
         row.appendChild(address);
         row.appendChild(phoneNum);
-        row.appendChild(website);
+        // row.appendChild(website);
+
+        row2.appendChild(website);
 
         table.appendChild(row);
+        table.appendChild(row2);
+        table.appendChild(blankRow);
     });
 }
